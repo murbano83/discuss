ae8b64e8523e1d22811804395b73a524afb26c64
Manuel Urbano Martínez <murb83@gmail.com>
2017-10-04 09:29:47 +0200

    Logout

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -18,11 +18,17 @@ defmodule Discuss.AuthController do
     signin(conn, changeset)
   end
 
+  def signout(conn, _params) do
+    conn
+    |> put_flash(:info, "Goodbye!")
+    |> configure_session(drop: true)
+    |> redirect(to: topic_path(conn, :index))
+  end
+
   defp signin(conn, changeset) do
     case insert_or_update_user(changeset) do
       {:ok, user} ->
         conn
-        |> put_flash(:info, "Welcome back!")
         |> put_session(:user_id, user.id)
         |> redirect(to: topic_path(conn, :index))
       {:error, _reason} ->
