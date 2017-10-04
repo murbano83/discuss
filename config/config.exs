f1d45ab4cdb23eb63a0014472eea815a75dc0bdd
Manuel Urbano Martínez <murb83@gmail.com>
2017-10-04 08:48:12 +0200

    signin with github

--- config/config.exs
+++ config/config.exs
@@ -25,3 +25,12 @@ config :logger, :console,
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: { Ueberauth.Strategy.Github, [] }
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+  client_id: "393a6d83e9f59e85a652",
+  client_secret: "4adfede7e282c64fd9a74f8999376654e9ff4abb"
